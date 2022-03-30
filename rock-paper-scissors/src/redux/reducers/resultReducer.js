9bda7fa710babe5a15985e56e1ab4a035ffc4883
David <davidsantmar@gmail.com>
2022-03-30 21:14:15 +0200

    added new sounds to final battle

--- rock-paper-scissors/src/redux/reducers/resultReducer.js
+++ rock-paper-scissors/src/redux/reducers/resultReducer.js
@@ -2,6 +2,9 @@ import resultActionTypes from "../actions/resultActionTypes";
 import youWinSound from '../sounds/you-win.mp3';
 import youLoseSound from '../sounds/you-lose.mp3';
 import drawSound from '../sounds/draw.mp3';
+import youWinPerfectSound from '../sounds/you-win-perfect.mp3';
+import loseSound from '../sounds/lose.mp3';
+
 function resultReducer(result = '', action) {
   switch (action.type) {
     case resultActionTypes.YOU_WIN:
@@ -21,9 +24,13 @@ function resultReducer(result = '', action) {
       return result = 'DRAW';
     case resultActionTypes.COMPUTER_WON:
       document.getElementById('result').style.backgroundColor = '#ED1D24';
+      const lose = new Audio(loseSound);
+      lose.play();
       return result = 'THE ENEMY HAS WON THE BATTLE';
     case resultActionTypes.PLAYER_WON:
       document.getElementById('result').style.backgroundColor = 'lightgreen';
+      const youWinPerfect = new Audio(youWinPerfectSound);
+      youWinPerfect.play();
       return result = 'YOU HAVE WON THE BATTLE';
     default:
       return result;
