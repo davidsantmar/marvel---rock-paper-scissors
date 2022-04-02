70ee5aafb8c82e84752b801800d849924b185b9c
David <davidsantmar@gmail.com>
2022-04-02 18:09:47 +0200

    added new media queries 768px

--- rock-paper-scissors/src/components/Header.jsx
+++ rock-paper-scissors/src/components/Header.jsx
@@ -6,15 +6,15 @@ import Result from './Result';
 const Header = () => {
     return (
         <div className='header'>
-            <div className="logo--container">
-                <div>
-                    <img className='logo' src={logo} alt='logo'/>
+                <div className="logo--container">
+                    <div>
+                        <img className='logo' src={logo} alt='logo'/>
+                    </div>
+                    <span className='subtitle'>ROCK PAPER SCISSORS</span>
                 </div>
-                <span className='subtitle'>ROCK PAPER SCISSORS</span>
-            </div>
-            <div className='result' id='result'>
-                <Result />
-            </div>
+                <div className='result' id='result'>
+                    <Result />
+                </div>
             <div className='score--container'>
                 <Score />
             </div>
