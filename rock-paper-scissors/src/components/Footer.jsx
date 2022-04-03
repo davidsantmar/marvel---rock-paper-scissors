3afad2dfb2da8c22a45e596de5aee144be41e12a
David <davidsantmar@gmail.com>
2022-04-03 15:08:02 +0200

    added footer test

--- rock-paper-scissors/src/components/Footer.jsx
+++ rock-paper-scissors/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footer--container'>
-            <footer>
+            <footer data-testid="footerText">
                 &nbsp;MADE WITH &#10084;&#65039; BY DAVID SANTIA<span className='go--container'>GO</span>!&nbsp;
             </footer>
         </div>
--- rock-paper-scissors/src/components/Footer.test.js
+++ rock-paper-scissors/src/components/Footer.test.js
@@ -3,13 +3,11 @@ import Footer from './Footer';
 
 describe('Given a footer component', () => {
     describe('When its rendered', () => {
-        beforeEach(() => {
-            render(<Footer />);
-        })
+        render(<Footer />);
         test('Then footer should be in the doc', () => {
-            const footer = screen.getByText("Made with ❤️ by David SantiaGO!");
-
-            expect(footer).toBeInTheDocument();
+            const footerText = screen.getByTestId('footerText');
+            expect(footerText).toBeInTheDocument();
         })
+
     })
 })
